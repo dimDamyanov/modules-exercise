92bece4dfd65e24dd6e6510404938b44229a784c
Dimitar I. Damyanov <dim.damianov@gmail.com>
2021-02-04 22:01:25 +0200

    Add main functionality

--- gui_shop/authentication.py
+++ gui_shop/authentication.py
@@ -1,30 +1,38 @@
-from tkinter import Button, Entry
+from tkinter import Button, Entry, Label
 from gui_shop.canvas import tk
 from gui_shop.helpers import clear_screen
+from gui_shop.products import render_products
 from json import dumps
-import os
 
 
-def login():
+def login(username, password):
+    with open('gui_shop/db/user_credentials_db.txt', 'r') as file:
+        lines = file.readlines()
+        for line in lines:
+            user_name, pass_word = line[:-1].split(', ')
+            if user_name == username and pass_word == password:
+                render_products()
+                return
+        render_login(errors=True)
     pass
 
 
 def register(**user):
-    print(os.getcwd())
-    files = [f for f in os.listdir()]
-    print(files)
-    print(user)
     user.update({'products': []})
     with open('gui_shop/db/users.txt', 'a') as file:
         file.write(dumps(user))
         file.write('\n')
+    with open('gui_shop/db/user_credentials_db.txt', 'a') as file:
+        file.write(f'{user.get("username")}, {user.get("password")}')
+        file.write('\n')
+    render_login()
 
 
 def render_register():
     clear_screen()
     username = Entry(tk)
     username.grid(row=0, column=0)
-    password = Entry(tk)
+    password = Entry(tk, show='*')
     password.grid(row=1, column=0)
     first_name = Entry(tk)
     first_name.grid(row=2, column=0)
@@ -37,13 +45,16 @@ def render_register():
                                     last_name=last_name.get())).grid(row=4, column=0)
 
 
-def render_login():
+def render_login(errors=None):
     clear_screen()
     username = Entry(tk)
     username.grid(row=0, column=0)
-    password = Entry(tk)
+    password = Entry(tk, show='*')
     password.grid(row=1, column=0)
-    Button(tk, text='Enter', bg='green', command=login).grid(row=2, column=0)
+    Button(tk, text='Enter', bg='green', command=lambda: login(username=username.get(),
+                                                               password=password.get())).grid(row=2, column=0)
+    if errors:
+        Label(tk, text='Invalid username or password').grid(row=3, column=0)
 
 
 def render_main_enter_screen():
---
+++ gui_shop/products.py
@@ -0,0 +1,25 @@
+from tkinter import Button, Label
+from gui_shop.helpers import clear_screen
+from gui_shop.canvas import tk
+from json import loads
+from PIL import Image, ImageTk
+import os
+base_folder = os.path.dirname(__file__)
+
+
+def render_products():
+    clear_screen()
+    with open('gui_shop/db/products.txt', 'r') as file:
+        products = file.readlines()
+        column_counter = 0
+        for product in products:
+            current_product = loads(product)
+            Label(text=current_product.get('name')).grid(row=0, column=column_counter)
+            image = Image.open(os.path.join(base_folder, 'images', current_product.get('img_name')))
+            image = image.resize((100, 100))
+            photo = ImageTk.PhotoImage(image)
+            img_label = Label(image=photo)
+            img_label.image = photo
+            img_label.grid(row=1, column=column_counter)
+            Button(tk, text=f'Buy {current_product.get("id")}').grid(row=2, column=column_counter)
+            column_counter += 1
